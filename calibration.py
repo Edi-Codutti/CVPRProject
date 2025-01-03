5d954b00ba6de80b0f6ca850f77921f1a7bd8883
Edi Codutti <edi.codutti@gmail.com>
2025-01-03 15:42:38 +0100

    Corrected projection error, forced K to be upper triangular, done some refactoring

--- calibration.py
+++ calibration.py
@@ -6,6 +6,9 @@ from scipy.spatial.transform import Rotation
 class Calibrator:
     def __init__(self, grid_size=(8,11)):
         self.grid_size = grid_size
+        self.reset()
+
+    def reset(self):
         self.homographies = []
         self.real_coords = None
         self.pix_coords = []
@@ -16,6 +19,7 @@ class Calibrator:
         self.rotations = []
         self.translations = []
         self._no_of_images = 0
+
         
     def estimate_homography(self, image)->tuple[np.ndarray, np.ndarray, np.ndarray]:
         return_value, corners = cv2.findChessboardCorners(image, patternSize=self.grid_size)
@@ -47,7 +51,7 @@ class Calibrator:
         H = h.reshape(3,3)
         return H, np.array(real_coords), np.array(pix_coords)
     
-    def estimate_projection_matrix(self, H:np.ndarray, K:np.ndarray)->np.ndarray:
+    def estimate_projection_matrix(self, H:np.ndarray, K:np.ndarray)->tuple[np.ndarray, np.ndarray, np.ndarray]:
         K_inv = np.linalg.inv(K)
         h1 = H[:, 0]
         h2 = H[:, 1]
@@ -57,14 +61,12 @@ class Calibrator:
         r2 = lambdas * K_inv@h2
         t = lambdas * K_inv@h3
         t = t.reshape((-1, 1))
-        self.translations.append(t)
         r3 = np.cross(r1, r2)
         R = np.hstack((r1.reshape((-1, 1)), r2.reshape((-1, 1)), r3.reshape((-1, 1))))
-        self.rotations.append(R)
         U, _ , V_t = np.linalg.svd(R)
         R = U@V_t
         P = K@np.hstack((R, t))
-        return P
+        return P, R, t
     
     def reprojection_error (self, P:np.ndarray, R:np.ndarray, I:np.ndarray, radial:bool):
         epsilon_tot=0
@@ -171,22 +173,17 @@ class Calibrator:
         if radial_distortion is True:
             result = least_squares(fr, x0, method='lm')
             K, dist_par, R, t = self._unpack_parameters(result.x, True)
-            projections = []
-            for i in range(self._no_of_images):
-                U, _ , V_t = np.linalg.svd(R[i])
-                R[i] = U@V_t
-                P = K@np.hstack((R[i], t[i].reshape(-1,1)))
-                projections.append(P)
             self.distortion_parameters = dist_par
         else:
             result = least_squares(fnr, x0, method='lm')
             K, R, t = self._unpack_parameters(result.x, False)
-            projections = []
-            for i in range(self._no_of_images):
-                U, _ , V_t = np.linalg.svd(R[i])
-                R[i] = U@V_t
-                P = K@np.hstack((R[i], t[i].reshape(-1,1)))
-                projections.append(P)
+        
+        projections = []
+        for i in range(self._no_of_images):
+            U, _ , V_t = np.linalg.svd(R[i])
+            R[i] = U@V_t
+            P = K@np.hstack((R[i], t[i].reshape(-1,1)))
+            projections.append(P)
             
         self.K = K
         self.rotations = R
@@ -195,6 +192,7 @@ class Calibrator:
         return
  
     def fit(self, images:list[np.ndarray], radial_distortion=False, iterative=False):
+        self.reset()
         self._no_of_images = len(images)
         for image in images:
             H, real_coords, pix_coords = self.estimate_homography(image)
@@ -204,9 +202,14 @@ class Calibrator:
             self.homographies.append(H)
         V = self.calculate_V()
         self.estimate_K(V)        
-        for H in self.homographies:
-            P = self.estimate_projection_matrix(H, self.K)
+        for i,H in enumerate(self.homographies):
+            P, R, t = self.estimate_projection_matrix(H, self.K)
+            if P[2,3] < 0:
+                self.homographies[i] = -H
+                P, R, t = self.estimate_projection_matrix(-H, self.K)
             self.projection_matrices.append(P)
+            self.rotations.append(R)
+            self.translations.append(t)
         if radial_distortion is True:
             self.estimate_distortion()
         if iterative is True:
@@ -228,32 +231,34 @@ class Calibrator:
     def _pack_parameters(self, radial:bool):
         flattened_K = self.K.flatten()
         if radial is True:
-            x = np.append(flattened_K, self.distortion_parameters)
+            x = np.append(flattened_K[np.r_[0:0+3,4,5]], self.distortion_parameters)
         else:
-            x = flattened_K
+            x = flattened_K[np.r_[0:0+3,4,5]]
         for i in range(self._no_of_images):
             r = Rotation.from_matrix(self.rotations[i])
             r = r.as_mrp()
             x = np.append(x,r)
             t = self.translations[i]
             x = np.append(x,t)
-        return np.array(x)
+        return x
     
     def _unpack_parameters(self, x:np.ndarray, radial:bool):
-        flattened_K = x[0:9]
-        K = flattened_K.reshape((3,3))
+        flattened_K = x[0:5]
+        K = np.eye(3,3)
+        K[0,:] = flattened_K[0:0+3]
+        K[1,1:] = flattened_K[3:]
         R = []
         t = []
         if radial is True:
-            distortion_params = x[9:11]
-            for i in range(11, x.shape[0], 6):
+            distortion_params = x[5:7]
+            for i in range(7, x.shape[0], 6):
                 rot = x[i:i+3]
                 rot = Rotation.from_mrp(rot)
                 R.append(rot.as_matrix())
                 t.append(x[i+3:i+6])
             return K, distortion_params, R, t
         else:
-            for i in range(9, x.shape[0], 6):
+            for i in range(5, x.shape[0], 6):
                 rot = x[i:i+3]
                 rot = Rotation.from_mrp(rot)
                 R.append(rot.as_matrix())
